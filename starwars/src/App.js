abd64ddbdd6a3881bd98468bfc1dc6b1d1545b4f
Awa Melvine <melvineawa@gmail.com>
2019-05-31 10:37:42 +0100

    Style app component

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -15,14 +15,12 @@ class App extends Component {
   }
 
   getCharacters = URL => {
-    // feel free to research what this code is doing.
-    // At a high level we are calling an API to fetch some starwars data from the open web.
-    // We then take that data and resolve it our state.
     fetch(URL)
       .then(res => {
         return res.json();
       })
       .then(data => {
+        console.log("---", data.results);
         this.setState({ starwarsChars: data.results });
       })
       .catch(err => {
@@ -32,10 +30,13 @@ class App extends Component {
 
   render() {
     const { starwarsChars } = this.state;
+
     return (
       <div className="App">
-        <h1 className="Header">React Wars</h1>
-        <StarwarsCharList starwarsChars={starwarsChars} />
+        <div className="starwars-container">
+          <h1 className="Header">React Wars</h1>
+          <StarwarsCharList starwarsChars={starwarsChars} />
+        </div>
       </div>
     );
   }
--- starwars/src/components/StarWarsChar.js
+++ starwars/src/components/StarWarsChar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./StarWars.css";
 
 const StarWarsChar = ({ char }) => (
   <div className="starwar-char">{char.name}</div>
