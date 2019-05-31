23b7dddb787f660260c32bf0d9533ed17b3c9b12
Awa Melvine <melvineawa@gmail.com>
2019-05-31 11:39:00 +0100

    Toggle display of each character's details on click

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -20,12 +20,29 @@ class App extends Component {
         return res.json();
       })
       .then(data => {
-        console.log("---", data.results);
-        this.setState({ starwarsChars: data.results });
+        const chars = data.results.map(char => ({
+          ...char,
+          expand: false,
+          id: Date.now()
+        }));
+        this.setState({ starwarsChars: chars });
       })
       .catch(err => {
         throw new Error(err);
       });
+  };
+
+  expandDetails = async char => {
+    const { starwarsChars } = this.state;
+    const updatedChars = starwarsChars.map(item => {
+      if (char.created === item.created) {
+        return { ...item, expand: !item.expand };
+      }
+      return item;
+    });
+    await this.setState({
+      starwarsChars: updatedChars
+    });
   };
 
   render() {
@@ -35,7 +52,10 @@ class App extends Component {
       <div className="App">
         <div className="starwars-container">
           <h1 className="Header">React Wars</h1>
-          <StarwarsCharList starwarsChars={starwarsChars} />
+          <StarwarsCharList
+            starwarsChars={starwarsChars}
+            expandDetails={this.expandDetails}
+          />
         </div>
       </div>
     );
--- starwars/src/components/StarwarsCharList.js
+++ starwars/src/components/StarwarsCharList.js
@@ -2,10 +2,14 @@ import React from "react";
 import "./StarWars.css";
 import StarWarsChar from "./StarWarsChar";
 
-const StarWarsCharList = ({ starwarsChars }) => (
+const StarWarsCharList = ({ starwarsChars, expandDetails }) => (
   <div className="starwars-charlist">
     {starwarsChars.map(char => (
-      <StarWarsChar key={char.created} char={char} />
+      <StarWarsChar
+        key={char.created}
+        char={char}
+        expandDetails={expandDetails}
+      />
     ))}
   </div>
 );
